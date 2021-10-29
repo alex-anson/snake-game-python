06c8bb58e518721e6f611741c5e032c22f544e34
Alex Anson <1alex.anson@gmail.com>
2021-10-29 18:11:36 -0400

    End of Python Day 2. :fairy_woman:

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## SNAKE GAME
 
 Day 2 of Python learning, decided to learn by making some simple games. This is just the snake game that was on the Nokia 20 years ago. I'm ready to have some fun!
+
+Was just in the comments section of the YT tutorial I'm following for this... one guy hit the nail on the head: It's not so much a tutorial as it is a "watch me code this." Lol...
+
+It's a good first project though. So what if I need to google a bunch of stuff?!
+
+Will finish on day 3 of Python. 🐍✨
--- main.py
+++ main.py
@@ -1,8 +1,53 @@
 import curses
 
-# setup window
+# SETUP WINDOW
 curses.initscr()  # init screen
 win = curses.newwin(20, 60, 0, 0)  # (lines, columns, y, x) ... usually
                                     # it's (x,y) for coordinates
 win.keypad(1)  # Lets the program know we want to use the arrow keys
-curses.noecho()  # Don't want
+curses.noecho()  # Don't want other keypresses to show up in the terminal
+curses.curs_set(0)  # Hide the cursor
+win.border(0)  # Draw a border
+win.nodelay(1)  # (no delay)  docs say: "if flag is True, getch()
+                # will be non-blocking"   getch = get character
+
+
+# SNAKE & FOOD (Coordinates)
+snake = [(4, 10), (4, 9), (4, 8)]
+food = (10, 20)
+
+# GAME LOGIC
+score = 0
+
+# Game should run unless user presses escape key.
+ESC = 27  # Define a constant with uppercase
+key = curses.KEY_RIGHT  # Starting by moving the snake to the right
+
+while key != ESC:
+    win.addstr(0, 2, 'Score: ' + str(score) + ' ')  # Add a string
+                            # (3rd argument) at 0,2 (y,x). (no idea why
+                            # there's a space after the score variable)
+    win.timeout(150 - (len(snake)) // 5 + len(snake) // 10 % 120)  # Increase speed
+
+    prev_key = key
+    event = win.getch()  # Wait for the next user input
+    key = event if event != -1 else prev_key
+    # ^ "if event (user input) is not -1 (he referenced the nodelay method but
+    # i've no idea what he's talking about), otherwise, use the previous key,
+    # continuing in the same direction"
+
+    # Check if it's one of the arrow keys
+    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
+        key = prev_key
+
+    # Calculate the next coordinates for the snake, starting with the head
+    y = snake[0][0]
+
+    for s in snake:
+        win.addch(s[0], s[1], '*')  # (y, x, character to use)
+
+    win.addch(food[0], food[1], '#')
+
+# SHUT THE GAME DOWN
+curses.endwin()  # Destroys the window
+print(f"Final score: {score}")
